eb37a33d1f77b8abbaa706e86789a806b1a43ebb
Ivan Bondar <ivan.bondar@kohls.com>
2014-10-24 21:45:33 +0400

    temp fix

--- integralUIprototype/src/main/webapp/js/call.js
+++ integralUIprototype/src/main/webapp/js/call.js
@@ -1,6 +1,6 @@
 // backbone example
 var CampaignDetailsModel = Backbone.Model.extend({
-    urlRoot: document.URL + 'rest/campaign/list',
+    urlRoot: 'rest/campaign/list',
 
     initialize: function () {
         console.log('Campaing Details Model has been created');
