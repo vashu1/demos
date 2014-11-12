2055a26d739ae0a16758546ca20ebd50ceee3c0a
snovikov <synopticum@gmail.com>
2014-11-12 17:32:27 +0300

    for qa

--- integralUIprototype/src/main/webapp/js/call.js
+++ integralUIprototype/src/main/webapp/js/call.js
@@ -1,6 +1,6 @@
 // backbone example
 var CampaignDetailsModel = Backbone.Model.extend({
-    urlRoot: '/rest/campaign/list',
+    urlRoot: '/home/campaigns/12345/campaign-details/rest/campaign/list',
 
     initialize: function () {
         console.log('Campaing Details Model has been created');
